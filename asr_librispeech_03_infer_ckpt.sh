49e94f565cf0526d10e6d1a09cbb578ce60183b6
Jungi Kim <jungi.kim@systrangroup.com>
2018-11-27 13:59:45 -0800

    Add Transformer ASR model with Librispeech dataset

--- asr_librispeech_03_infer_ckpt.sh
+++ asr_librispeech_03_infer_ckpt.sh
@@ -6,9 +6,9 @@ if [ -z "$CHECKPOINT_STEP" ] ; then
   exit 1
 fi
 
-if [ ! -f librispeech_model/model.ckpt-${CHECKPOINT_STEP} ] ; then
+if [ ! -f librispeech_model/model.ckpt-${CHECKPOINT_STEP}.index ] ; then
   echo "Checkpoint librispeech_model/model.ckpt-${CHECKPOINT_STEP} does not exist"
   exit 1
 fi
 
-CUDA_VISIBLE_DEVICES=0 PYTHONPATH=. python2 opennmt/bin/main.py --config config/data/librispeech.yml config/asr_transformer.yml --model_type TransformerASR --features_file librispeech_data/test-clean-flacs.tfrecords --checkpoint_path librispeech_model/model.ckpt-${CHECKPOINT_STEP} --num_gpus 1 infer asr_librispeech_infer_test-clean_ckpt-${CHECKPOINT_STEP}.txt 2> asr_librispeech_infer_test-clean_ckpt-${CHECKPOINT_STEP}.log &
+CUDA_VISIBLE_DEVICES=0 PYTHONPATH=. python2 opennmt/bin/main.py --config config/data/librispeech.yml config/asr_transformer.yml --model_type TransformerASR --features_file librispeech_data/test-clean-flacs.tfrecords --checkpoint_path librispeech_model/model.ckpt-${CHECKPOINT_STEP} --num_gpus 1 infer > asr_librispeech_infer_test-clean_ckpt-${CHECKPOINT_STEP}.txt 2> asr_librispeech_infer_test-clean_ckpt-${CHECKPOINT_STEP}.log &
